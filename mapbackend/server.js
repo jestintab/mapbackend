081559637fb3ff719df5179cfb6de4f100afd9e7
Jestin Abraham <jestintab@Jestins-MacBook-Pro.local>
2020-07-20 13:26:23 +0300

    socketio added

--- mapbackend/server.js
+++ mapbackend/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const server = require('http').createServer();
+const io = require('socket.io')(server);
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-
+//const socket = io();
 app.use(cors());
 app.use(express.json());
 
@@ -24,6 +25,16 @@ const driversRouter = require('./routes/drivers');
 app.use('/drivers', driversRouter);
 
 
+
+io.on('connection', client => {
+    console.log('a user connected');
+    client.on('position', data => { 
+        client.broadcast.emit('location',{data})
+       // console.log(data);
+    });
+    client.on('disconnect', () => { /* … */ });
+  });
+  server.listen(5100);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
